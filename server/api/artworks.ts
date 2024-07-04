caf2ad2f9cd785436a4bccbda76a15b5ae210fb5
yuuraeru <danialananto@protonmail.com>
2024-07-05 03:32:28 +0700

    pray for bug fixes (2)

--- server/api/artworks.ts
+++ server/api/artworks.ts
@@ -1,6 +1,5 @@
 import fs from 'node:fs'
 import path from 'node:path'
-import { join } from 'path'
 
 export default defineEventHandler(async (event) => {
   const page = parseInt(getQuery<{ page: string }>(event).page || '1')
@@ -8,19 +7,22 @@ export default defineEventHandler(async (event) => {
 
   const startIndex = (page - 1) * limit
   const endIndex = page * limit
-  const folderPath = join(process.cwd(), 'public/assets/artworks')
-  const files = await fs.readdirSync(folderPath).reverse()
+  const folderPath = path.join(process.cwd(), 'public', 'assets', 'artworks')
+  try {
+    const files = fs.readdirSync(folderPath).reverse()
+    const res = {
+      total: files.length,
+      limit: limit,
+      currentPage: page,
+      lastPage: Math.ceil(files.length / limit),
+      firstPage: 1,
+      nextPage: endIndex < files.length ? page + 1 : null,
+      prevPage: startIndex > 0 ? page - 1 : null,
+      data: files.slice(startIndex, endIndex),
+    }
 
-  const res = {
-    total: files.length,
-    limit: limit,
-    currentPage: page,
-    lastPage: Math.ceil(files.length / limit),
-    firstPage: 1,
-    nextPage: endIndex < files.length ? page + 1 : null,
-    prevPage: startIndex > 0 ? page - 1 : null,
-    data: files.slice(startIndex, endIndex),
+    return res
+  } catch (error) {
+    return error
   }
-
-  return res
 })
