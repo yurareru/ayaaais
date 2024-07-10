781fbf18b474613ef70d4bd079de0829fa208ae0
yuuraeru <danialananto@protonmail.com>
2024-07-10 21:43:28 +0700

    revert everything

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,28 +9,10 @@ export default defineNuxtConfig({
     '@formkit/auto-animate/nuxt',
   ],
 
-  // runtimeConfig: {
-  //   proxy: 'https://yuuraeru-api.vercel.app/',
-  // },
-
-  routeRules: {
-    '/api/**': {
-      proxy: `https://yuuraeru-api.vercel.app/api/v1/**`,
-    },
-    '/_image/**': {
-      proxy: `https://yuuraeru-api.vercel.app/_image/**`,
-    },
-    '/assets/artworks/**': {
-      proxy: `https://yuuraeru-api.vercel.app/artworks/**`,
-    },
-  },
-
   image: {
     format: ['webp'],
-    domains: ['yuuraeru-api.vercel.app'],
     alias: {
-      artworks: 'https://yuuraeru-api.vercel.app/artworks',
-      // artworks: '/assets/artworks',
+      artworks: '/assets/artworks',
     },
   },
 
